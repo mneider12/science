c60f1bc363cb9953772d02361aaf33acebc55c3f
Mark Neider <8540826+mneider12@users.noreply.github.com>
2025-04-12 17:15:58 -0700

    Parameterize symbol display test

--- models/build.gradle.kts
+++ models/build.gradle.kts
@@ -1,3 +1,7 @@
 plugins {
     id("buildlogic.kotlin-library-conventions")
 }
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.4")
+}
--- models/src/test/kotlin/com/nydev/models/atom/SymbolTest.kt
+++ models/src/test/kotlin/com/nydev/models/atom/SymbolTest.kt
@@ -1,10 +1,36 @@
 package com.nydev.models.atom
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 
 class SymbolTest {
-    @Test fun display_H_returnsH() {
-        assertEquals("H", Symbol.H.display)
+
+    @ParameterizedTest
+    @EnumSource()
+    fun display_returns(symbol: Symbol) {
+        assertEquals(
+            when (symbol) {
+                Symbol.H -> "H"
+                Symbol.HE -> "He"
+                Symbol.LI -> "Li"
+                Symbol.BE -> "Be"
+                Symbol.B -> "B"
+                Symbol.C -> "C"
+                Symbol. N -> "N"
+                Symbol.O -> "O"
+                Symbol.F -> "F"
+                Symbol.NE -> "Ne"
+                Symbol.NA -> "Na"
+                Symbol.MG -> "Mg"
+                Symbol.AL -> "Al"
+                Symbol.SI -> "Si"
+                Symbol.P -> "P"
+                Symbol.S -> "S"
+                Symbol.CL -> "Cl"
+                Symbol.AR -> "Ar"
+            },
+            symbol.display
+        )
     }
 }
