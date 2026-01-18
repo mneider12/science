5aa23a2393d9ca6747b9e79a3c0c26a96595330e
Mark Neider <8540826+mneider12@users.noreply.github.com>
2026-01-17 19:44:04 -0800

    Add remaining symbols up to element 118

--- models/src/main/kotlin/com/nydev/models/atom/Symbol.kt
+++ models/src/main/kotlin/com/nydev/models/atom/Symbol.kt
@@ -1,7 +1,16 @@
 package com.nydev.models.atom
 
 enum class Symbol(val display: String) {
-    H("H"), HE("He"), LI("Li"), BE("Be"), B("B"), C("C"), N("N"),
-    O("O"), F("F"), NE("Ne"), NA("Na"), MG("Mg"), AL("Al"), SI("Si"),
-    P("P"), S("S"), CL("Cl"), AR("Ar"),
+    H("H"), HE("He"), LI("Li"), BE("Be"), B("B"), C("C"), N("N"), O("O"), F("F"), NE("Ne"),
+    NA("Na"), MG("Mg"), AL("Al"), SI("Si"), P("P"), S("S"), CL("Cl"), AR("Ar"), K("K"), CA("Ca"),
+    SC("Sc"), TI("Ti"), V("V"), CR("Cr"), MN("Mn"), FE("Fe"), CO("Co"), NI("Ni"), CU("Cu"), ZN("Zn"),
+    GA("Ga"), GE("Ge"), AS("As"), SE("Se"), BR("Br"), KR("Kr"), RB("Rb"), SR("Sr"), Y("Y"), ZR("Zr"),
+    NB("Nb"), MO("Mo"), TC("Tc"), RU("Ru"), RH("Rh"), PD("Pd"), AG("Ag"), CD("Cd"), IN("In"), SN("Sn"),
+    SB("Sb"), TE("Te"), I("I"), XE("Xe"), CS("Cs"), BA("Ba"), LA("La"), CE("Ce"), PR("Pr"), ND("Nd"),
+    PM("Pm"), SM("Sm"), EU("Eu"), GD("Gd"), TB("Tb"), DY("Dy"), HO("Ho"), ER("Er"), TM("Tm"), YB("Yb"),
+    LU("Lu"), HF("Hf"), TA("Ta"), W("W"), RE("Re"), OS("Os"), IR("Ir"), PT("Pt"), AU("Au"), HG("Hg"),
+    TL("Tl"), PB("Pb"), BI("Bi"), PO("Po"), AT("At"), RN("Rn"), FR("Fr"), RA("Ra"), AC("Ac"), TH("Th"),
+    PA("Pa"), U("U"), NP("Np"), PU("Pu"), AM("Am"), CM("Cm"), BK("Bk"), CF("Cf"), ES("Es"), FM("Fm"),
+    MD("Md"), NO("No"), LR("Lr"), RF("Rf"), DB("Db"), SG("Sg"), BH("Bh"), HS("Hs"), MT("Mt"), DS("Ds"),
+    RG("Rg"), CN("Cn"), NH("Nh"), FL("Fl"), MC("Mc"), LV("Lv"), TS("Ts"), OG("Og"),
 }
--- models/src/test/kotlin/com/nydev/models/atom/SymbolTest.kt
+++ models/src/test/kotlin/com/nydev/models/atom/SymbolTest.kt
@@ -9,28 +9,7 @@ class SymbolTest {
     @ParameterizedTest
     @EnumSource()
     fun display_returns(symbol: Symbol) {
-        assertEquals(
-            when (symbol) {
-                Symbol.H -> "H"
-                Symbol.HE -> "He"
-                Symbol.LI -> "Li"
-                Symbol.BE -> "Be"
-                Symbol.B -> "B"
-                Symbol.C -> "C"
-                Symbol. N -> "N"
-                Symbol.O -> "O"
-                Symbol.F -> "F"
-                Symbol.NE -> "Ne"
-                Symbol.NA -> "Na"
-                Symbol.MG -> "Mg"
-                Symbol.AL -> "Al"
-                Symbol.SI -> "Si"
-                Symbol.P -> "P"
-                Symbol.S -> "S"
-                Symbol.CL -> "Cl"
-                Symbol.AR -> "Ar"
-            },
-            symbol.display
-        )
+        val expected = symbol.name.lowercase().replaceFirstChar { it.uppercase() }
+        assertEquals(expected, symbol.display)
     }
 }
